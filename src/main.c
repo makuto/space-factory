adee277f9f9e4952b686c2bf164569f41893d8a4
Macoy Madson <macoy@macoy.me>
2022-06-15 20:13:49 -0400

    Start doing tiles

--- src/main.c
+++ src/main.c
@@ -1,6 +1,68 @@
+#include <assert.h>
 #include <stdio.h>
+
 #include "SDL.h"
+
+// From Cakelisp
 #include "SDL.cake.hpp"
+
+typedef unsigned char GridCell;
+
+struct GridSpace
+{
+	unsigned char width;
+	unsigned char height;
+	GridCell* data;
+};
+
+#define GridCellAt(gridSpace, x, y) gridSpace->data[(y * gridSpace->width) + x]
+
+static GridSpace* createGridSpace(unsigned char width, unsigned char height)
+{
+	char* mem = (char*)malloc(sizeof(GridSpace) + (width * height * sizeof(GridCell)));
+	GridSpace* newSpace = (GridSpace*)mem;
+	newSpace->width = width;
+	newSpace->height = height;
+	newSpace->data = (GridCell*)(mem + sizeof(GridSpace));
+	return newSpace;
+}
+
+static void freeGridSpace(GridSpace* gridSpace)
+{
+	free(gridSpace);
+}
+
+static void renderGridSpace(GridSpace* gridSpace)
+{
+	for (int y = 0; y < gridSpace->height; ++y)
+	{
+		for (int x = 0; x < gridSpace->width; ++x)
+		{
+			// draw...
+			fprintf(stderr, "%c", GridCellAt(gridSpace, x, y));
+		}
+		fprintf(stderr, "\n");
+	}
+}
+
+static void setGridSpaceFromString(GridSpace* gridSpace, const char* str)
+{
+	GridCell* writeHead = gridSpace->data;
+	GridCell* gridSpaceEnd = writeHead + (gridSpace->width * gridSpace->height);
+	for (const char* c = str; *c != 0; ++c)
+	{
+		if (*c == '\n')
+			continue;
+
+		assert(writeHead < gridSpaceEnd && "GridSpace doesn't have enough room to fit the string.");
+		*writeHead = *c;
+		++writeHead;
+	}
+}
+
+//
+// Main
+//
 
 int main(int numArguments, char** arguments)
 {
@@ -11,6 +73,21 @@ int main(int numArguments, char** arguments)
 		return 1;
 	}
 	const char* exitReason = NULL;
+
+	GridSpace* playerShip = createGridSpace(20, 9);
+
+	setGridSpaceFromString(playerShip,
+	                       "...................."
+	                       ".##################."
+	                       ".#................#."
+	                       ".#................#."
+	                       ".#................#."
+	                       ".#................#."
+	                       ".#................#."
+	                       ".##################."
+	                       "....................");
+
+	renderGridSpace(playerShip);
 
 	while (!(exitReason))
 	{
@@ -27,6 +104,7 @@ int main(int numArguments, char** arguments)
 		{
 			exitReason = "Escape pressed";
 		}
+
 		SDL_UpdateWindowSurface(window);
 	}
 
